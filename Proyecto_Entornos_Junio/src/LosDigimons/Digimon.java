197bd3fa614a6c12405e5b37f9f03514172254c8
Andrea Catalán Menacho <145468932+4ndreaCatMen@users.noreply.github.com>
2024-05-28 18:28:24 +0200

    Creacion clase digimons
    atributos

--- Proyecto_Entornos_Junio/src/LosDigimons/Digimon.java
+++ Proyecto_Entornos_Junio/src/LosDigimons/Digimon.java
@@ -1,6 +1,14 @@
 package LosDigimons;
+
 import java.util.Random;
 
 public class Digimon {
-    int nume;
+    private String nombre;
+    private int nivel;
+    private int puntosAtaque;
+    private int puntosSalud;
+    private int dp1;
+    private int dp2;
+
 }
+
