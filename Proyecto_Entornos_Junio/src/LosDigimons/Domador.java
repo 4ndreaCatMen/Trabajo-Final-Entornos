98b9213c64c0a7a9bd6604722d898a929b75a892
Andrea Catalán Menacho <145468932+4ndreaCatMen@users.noreply.github.com>
2024-05-29 15:32:50 +0200

    metodos clase domador

--- Proyecto_Entornos_Junio/src/LosDigimons/Domador.java
+++ Proyecto_Entornos_Junio/src/LosDigimons/Domador.java
@@ -17,4 +17,23 @@ import Pruebas_digimons.Digimons;
 	        this.equipo.add(new Digimon("Gabumon"));
 	        this.equipo.add(new Digimon("Patamon"));
 }
+	    // creacion de metodos
+	    
+	    public Digimon crearDigimonAleatorio() {
+	        String[] nombres = {"Piyomon", "Tentomon", "Biyomon", "Palmon", "Gatomon", "Gomamon", "Tailmon"};
+	        String nombreAleatorio = nombres[new Random().nextInt(nombres.length)];
+	        return new Digimon(nombreAleatorio);
+	    }
+
+	    public void capturar(Digimon digimon) {
+	        boolean enemigoYaEstaEnEquipo = equipo.contains(digimon);
+
+	        if (!enemigoYaEstaEnEquipo && digimon.getPuntosSalud() <= 20) {
+	            equipo.add(digimon);
+	            System.out.println(digimon.getNombre() + " se ha unido a su equipo");
+	        } else {
+	            System.out.println("No se puede unir");
+	        }
+	    }
+
 }
