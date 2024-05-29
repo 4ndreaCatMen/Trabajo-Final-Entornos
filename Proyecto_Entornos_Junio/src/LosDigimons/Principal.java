661eca275f67275fcf90c9fc005ca9792c6fb15b
Andrea Catalán Menacho <145468932+4ndreaCatMen@users.noreply.github.com>
2024-05-29 17:13:22 +0200

    Etiquetas clase Principal ( se me había olvidado)

--- Proyecto_Entornos_Junio/src/LosDigimons/Principal.java
+++ Proyecto_Entornos_Junio/src/LosDigimons/Principal.java
@@ -2,21 +2,36 @@ package LosDigimons;
 
 import java.util.Scanner;
 
+/**
+ * Clase que contiene el punto de entrada del programa y controla el flujo principal del juego.
+ * Permite al jugador elegir un domador y sus acciones, como iniciar una batalla.
+ * 
+ * @author Andrea
+ */
+
 public class Principal {
+	
+    /**
+     * Comienzo del programa.
+     * 
+     * @param args Los argumentos de la línea de comandos (no se utilizan en este caso).
+     */
+
+	
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         // Lista de nombres de domadores
         String[] nombresDomadores = {"Tai Yagami", "Yamato Ishida", "Sora Takenouchi", "Koshiro Izumi", "Mimi Tachikawa", "Joe Kido", "Takeru Takaishi", "Hikari Yamagi"};
 
-        // Mostrar opciones de domadores y permitir al usuario elegir uno
+        // Mostrar opciones de domadores y permitir al usuario elegir unx
         System.out.println("Escoge el domador/a que quieres usar:");
         for (int i = 0; i < nombresDomadores.length; i++) {
             System.out.println((i + 1) + ". " + nombresDomadores[i]);
         }
         int eleccionDomador = scanner.nextInt() - 1;
 
-        // Crear el domador elegido
+        // Crear el domador/a elegido
         Domador domador = new Domador(nombresDomadores[eleccionDomador]);
         
      // Menú principal
@@ -26,7 +41,9 @@ public class Principal {
             int opcion = scanner.nextInt();
 
             if (opcion == 1) {
-                
+            	
+            	// Elige el Digimon antes de iniciar la batalla
+            	
                 Digimon digimonElegido = elegirDigimon(domador, scanner);
                 BatallaDigital batalla = new BatallaDigital(domador, digimonElegido);
                 batalla.pelea();
@@ -40,6 +57,14 @@ public class Principal {
         scanner.close();
     }
 
+    /**
+     * Permite al jugador elegir un Digimon de su equipo para la batalla.
+     * 
+     * @param domador: El domador que posee el equipo de Digimons.
+     * @param scanner: El objeto Scanner para la entrada del usuario.
+     * @return El Digimon elegido por el jugador.
+     */
+    
     private static Digimon elegirDigimon(Domador domador, Scanner scanner) {
         System.out.println("Elige el Digimon que quieres usar:");
         for (int i = 0; i < domador.getEquipo().size(); i++) {
