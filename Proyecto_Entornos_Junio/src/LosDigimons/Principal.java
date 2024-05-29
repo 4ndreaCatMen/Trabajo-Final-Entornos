1c93086d44c4a40c7b5416cadfe2704f57befe7c
Andrea Catalán Menacho <145468932+4ndreaCatMen@users.noreply.github.com>
2024-05-29 16:22:38 +0200

    Creación clase Principal

--- Proyecto_Entornos_Junio/src/LosDigimons/Principal.java
+++ Proyecto_Entornos_Junio/src/LosDigimons/Principal.java
@@ -3,5 +3,20 @@ import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
-}
-}
+        Scanner scanner = new Scanner(System.in);
+
+        // Lista de nombres de domadores
+        String[] nombresDomadores = {"Tai Yagami", "Yamato Ishida", "Sora Takenouchi", "Koshiro Izumi", "Mimi Tachikawa", "Joe Kido", "Takeru Takaishi", "Hikari Yamagi"};
+
+        // Mostrar opciones de domadores y permitir al usuario elegir uno
+        System.out.println("Escoge el domador/a que quieres usar:");
+        for (int i = 0; i < nombresDomadores.length; i++) {
+            System.out.println((i + 1) + ". " + nombresDomadores[i]);
+        }
+        int eleccionDomador = scanner.nextInt() - 1;
+
+        // Crear el domador elegido
+        Domador domador = new Domador(nombresDomadores[eleccionDomador]);
+
+    }
+    }
