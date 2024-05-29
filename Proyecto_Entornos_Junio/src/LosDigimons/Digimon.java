ee3a4e1a01620af6e776762357cdb22e69560f02
Andrea Catalán Menacho <145468932+4ndreaCatMen@users.noreply.github.com>
2024-05-29 12:12:09 +0200

    Completando parte ataques

--- Proyecto_Entornos_Junio/src/LosDigimons/Digimon.java
+++ Proyecto_Entornos_Junio/src/LosDigimons/Digimon.java
@@ -10,5 +10,29 @@ public class Digimon {
     private int dp1;
     private int dp2;
 
+    public Digimon(String nombre) {
+        this.nombre = nombre;
+        this.nivel = new Random().nextInt(5) + 1;
+        this.puntosAtaque = 5 * this.nivel;
+        this.puntosSalud = 10 * this.nivel;
+        this.dp1 = 10;
+        this.dp2 = 10;
+    }
+
+    public int ataque1() {
+        if (dp1 > 0) {
+            dp1--;
+            return puntosAtaque;
+        }
+        return 0;
+    }
+
+    public int ataque2() {
+        if (dp2 > 0) {
+            dp2 -= 2;
+            return 2 * puntosAtaque;
+        }
+        return 0;
+    }
 }
 
