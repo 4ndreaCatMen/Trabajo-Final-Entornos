71be880b86ee151a295b511f4f9e5c758487cdb1
Andrea Catalán Menacho <145468932+4ndreaCatMen@users.noreply.github.com>
2024-05-29 15:30:44 +0200

    Comienzo clase domador

--- Proyecto_Entornos_Junio/src/LosDigimons/Domador.java
+++ Proyecto_Entornos_Junio/src/LosDigimons/Domador.java
@@ -4,6 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class Domador {
-    
+import Pruebas_digimons.Digimons;
+
+	public class Domador {
+	    private String nombre;
+	    private List<Digimon> equipo;
+
+	    public Domador(String nombre) {
+	        this.nombre = nombre;
+	        this.equipo = new ArrayList<>();
+	        this.equipo.add(new Digimon("Agumon"));
+	        this.equipo.add(new Digimon("Gabumon"));
+	        this.equipo.add(new Digimon("Patamon"));
+}
 }
