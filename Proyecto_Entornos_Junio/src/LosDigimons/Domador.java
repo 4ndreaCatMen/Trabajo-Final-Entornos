e12e55cd8abd437b637babf0a8b3da309a27ef29
Andrea Catalán Menacho <145468932+4ndreaCatMen@users.noreply.github.com>
2024-05-29 17:01:25 +0200

    etiquetas clase Domador

--- Proyecto_Entornos_Junio/src/LosDigimons/Domador.java
+++ Proyecto_Entornos_Junio/src/LosDigimons/Domador.java
@@ -1,47 +1,86 @@
 package LosDigimons;
 
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-import Pruebas_digimons.Digimons;
+/**
+ * Clase que representa a un Domador de Digimons.
+ * Permite crear y gestionar un equipo de Digimons.
+ * 
+ * @author Andrea
+ */
 
-	public class Domador {
-	    private String nombre;
-	    private List<Digimon> equipo;
+//Aquí comienza la clase principal llamada Domador
+public class Domador {
+ // Aquí definimos las características principales del domador
+ private String nombre; // El nombre del domador
+ private List<Digimon> equipo; // La lista de Digimons que tiene en su equipo
 
-	    public Domador(String nombre) {
-	        this.nombre = nombre;
-	        this.equipo = new ArrayList<>();
-	        this.equipo.add(new Digimon("Agumon"));
-	        this.equipo.add(new Digimon("Gabumon"));
-	        this.equipo.add(new Digimon("Patamon"));
+ // Este es el constructor del Domador, que se ejecuta cuando creamos un nuevo Domador
+ public Domador(String nombre) {
+     this.nombre = nombre; // Guardamos el nombre del domador
+     this.equipo = new ArrayList<>(); // Creamos una nueva lista vacía para su equipo
+     // Añadimos algunos Digimons iniciales al equipo del domador
+     this.equipo.add(new Digimon("Agumon"));
+     this.equipo.add(new Digimon("Gabumon"));
+     this.equipo.add(new Digimon("Patamon"));
+ }
+
+ /**
+  * Crea un Digimon aleatorio.
+  * 
+  * @return Un Digimon con nombre aleatorio.
+  */
+ 
+ public Digimon crearDigimonAleatorio() {
+     // Definimos algunos nombres de Digimons
+     String[] nombres = {"Piyomon", "Tentomon", "Biyomon", "Palmon", "Gatomon", "Gomamon", "Tailmon"};
+     // Elegimos uno de los nombres aleatoriamente
+     String nombreAleatorio = nombres[new Random().nextInt(nombres.length)];
+     // Creamos un nuevo Digimon con ese nombre
+     return new Digimon(nombreAleatorio);
+ }
+
+ /**
+  * Captura un Digimon y lo añade al equipo del domador si cumple ciertas condiciones.
+  * 
+  * @param digimon El Digimon a capturar.
+  */
+ 
+ public void capturar(Digimon digimon) {
+     // Revisamos si el Digimon ya está en el equipo del domador
+     boolean enemigoYaEstaEnEquipo = equipo.contains(digimon);
+
+     // Si el Digimon no está en el equipo y tiene menos de 20 puntos de salud, lo añadimos al equipo
+     if (!enemigoYaEstaEnEquipo && digimon.getPuntosSalud() <= 20) {
+         equipo.add(digimon);
+         System.out.println(digimon.getNombre() + " se ha unido a su equipo");
+     } else {
+         // Si no cumple con las condiciones anteriores, no se puede unir al equipo
+         System.out.println("No se puede unir");
+     }
+ }
+
+ /**
+  * Obtiene el equipo de Digimons del domador.
+  * 
+  * @return El equipo de Digimons del domador.
+  */
+ 
+ public List<Digimon> getEquipo() {
+     return equipo;
+ }
+
+ /**
+  * Obtiene el nombre del domador.
+  * 
+  * @return El nombre del domador.
+  */
+ 
+ public String getNombre() {
+     return nombre;
+ }
 }
-	    // creacion de metodos
-	    
-	    public Digimon crearDigimonAleatorio() {
-	        String[] nombres = {"Piyomon", "Tentomon", "Biyomon", "Palmon", "Gatomon", "Gomamon", "Tailmon"};
-	        String nombreAleatorio = nombres[new Random().nextInt(nombres.length)];
-	        return new Digimon(nombreAleatorio);
-	    }
 
-	    public void capturar(Digimon digimon) {
-	        boolean enemigoYaEstaEnEquipo = equipo.contains(digimon);
-
-	        if (!enemigoYaEstaEnEquipo && digimon.getPuntosSalud() <= 20) {
-	            equipo.add(digimon);
-	            System.out.println(digimon.getNombre() + " se ha unido a su equipo");
-	        } else {
-	            System.out.println("No se puede unir");
-	        }
-	    }
-	    	// listado del equipo digimon con sus nombres
-
-	    public List<Digimon> getEquipo() {
-	        return equipo;
-	    }
-
-	    public String getNombre() {
-	        return nombre;
-	    }
-	}
